cf87cb447c583ad06a77c6516898cc45db9f5630
mohit Raj <mohitraj8475@gmail.com>
2026-08-28 19:51:06 +0530

    feat: add containerized application

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "DevOps Assignment Application",
+        "status": "running"
+    })
+
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
